ca8043ba33a195085378f8aeaa864c4d79f577e9
Rehman <rk03229975701@gmail.com>
2023-10-21 02:29:31 -0700

    adding a conditinals for if the enter the day <0 it is not allowed

--- dataplan.c
+++ dataplan.c
@@ -5,12 +5,19 @@ int main(){
     int remaining_days = 30;
     double totaldata = 30.0;
 
-    printf("days uesd: ");
+    printf("Days uesd: ");
     scanf("%d",&days);
 
     int a = remaining_days - days;
     printf("%d remaining days \n", a);
 
     double average = totaldata/days;
+    printf("Average used %lf GB/day \n",average);
+
+    ifO(average <=0 || days <30 || totaldata <0){
+        printf("ERROR:\n");
+    }else if(average > totaldata/30.0){
+        printf("ERROR: you re exceded today used %lf",totaldata/30.0);
+    }
     
 }
